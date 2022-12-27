ec3bb40abb767e04c63d565c38cc39c4f204aca2
Ronie Peterson <roniepetersondf@gmail.com>
2022-12-27 09:31:40 -0300

    Update settings.py

--- udemy/agenda/agenda/settings.py
+++ udemy/agenda/agenda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c__vw4949^*!9uddh^35r-jyuu-cjhs5jve$!ija0-9(pq^*mj'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
