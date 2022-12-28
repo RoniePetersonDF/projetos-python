ca3178471f4238b392af9da372bbc2a530d1ddcb
RoniePetersonDF <roniepetersondf@gmail.com>
2022-12-28 14:00:25 -0300

    mod3cap4-Buscando receitas

--- alura/alura_receita/receitas/urls.py
+++ alura/alura_receita/receitas/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from receitas.views import index, receita
+from receitas.views import index, receita, buscar
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('<int:receita_id>', receita, name='receita'),
-]
+    path('buscar', buscar, name='buscar'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
