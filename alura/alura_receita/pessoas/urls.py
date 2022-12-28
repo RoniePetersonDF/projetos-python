b1f029b905ccbc277e468a126e536cccf1935de6
RoniePetersonDF <roniepetersondf@gmail.com>
2022-12-28 12:58:51 -0300

    mod3cap02-Quem postou a receita

---
+++ alura/alura_receita/pessoas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from pessoas.views import index
+
+urlpatterns = [
+    path('', index, name='pessoas'),
+]
+
+
--- alura/alura_receita/setup/urls.py
+++ alura/alura_receita/setup/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('receitas.urls')),
+    path('pessoas/', include('pessoas.urls')),
     path('admin/', admin.site.urls),
 ]
